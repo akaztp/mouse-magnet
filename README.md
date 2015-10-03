6e8b6de964ff08ff351d2b2e811d1945973378a5
José Proença <joseantonioproenca@gmail.com>
2015-10-03 09:42:48 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mouse-magnet
-Attract and repel with your pointer
+Attract and repel with your pointer.
+
+This code setup a physics model and takes care of updating the html objects' positions.
+
---
+++ mouse-magnet/test/ztp-physics.js
@@ -0,0 +1,30 @@
+﻿$(window).load(function () {
+	'use strict';
+
+	var config =
+	{
+		drag: 0.6,
+		particleSelector: '#ztp-photo li img',
+		boundsSelector: '#Area3',
+		attraction: false,
+		autoPoke: 5, // in seconds
+		lowCpuDetect: true
+	}
+
+	var mm = new MouseMagnet(config);
+	mm.start();
+
+	$('#ztp-photo').mouseleave(shuffle);
+
+	function shuffle(evt)
+	{
+		var seed = Math.floor(Math.random() * 4);
+		$('#ztp-photo li').each(
+			function (idx, elem)
+			{
+				$(elem).css('left', ((idx + seed) >> 1) % 2 ? 64 : 0).css('top', ((idx + seed) % 2) ? 0 : 64);
+			});
+		mm.reposition();
+	}
+
+});
